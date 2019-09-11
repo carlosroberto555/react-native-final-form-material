489add91434dc099f0b3443e602aa01b373983b4
Carlos Roberto <carlos.parreira@sige.pro.br>
2019-09-11 09:23:52 -0300

    export global

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as Input } from './Input'
-export { default as TextArea } from './TextArea'
-export { default as RadioButton } from './RadioButton'
+export { default as Input, InputProps } from './Input'
+export { default as TextArea, TextAreaProps } from './TextArea'
+export { default as RadioButton, RadioButtonsProps } from './RadioButton'
